5a8d53d04511f1381fbc8abd42c6a1727ad0c19c
Rômulo <romulo.deyvid@hotmail.com>
2024-01-23 08:53:33 -0300

    Adicionando arquivos do jogo

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,22 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const elementoChute = document.querySelector('#chute')
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    console.log(e)
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
+}
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${chute}</span>
+    `
+}
+recognition.addEventListener('end', () => recognition.start())
